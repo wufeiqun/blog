8623af1539445e2a9b4c929207fa19d224a9aaf6
rocky <rocky@workubuntu.com>
2015-05-29 12:33:07 +0800

    suanfa

--- algorithm/1.字符串反转.py
+++ algorithm/1.字符串反转.py
@@ -23,13 +23,28 @@ def second(stra):
 	print '原始字符串:',stra
 	print '旋转后的字符串:',strb
 
+#方法三,利用对半替换
 def third(stra):
+    lst = stra.split()
+    half = len(lst) / 2
+    if len(lst) == 0:
+        print 'List can\'t be None.'
+    elif len(lst) == 1:
+        print ' '.join(lst)
+    else:
+        i = 1
+        while i <= half:
+            lst[i - 1],lst[-i] = lst[-i],lst[i - 1]
+            i+=1
+        print '反转后的结果是:',' '.join(lst)
+
 	
-
-
 if __name__ =='__main__':
 	stra = 'this is a desk'
 	print '------------------------------------方法一-------------------------------------'
 	first(stra)
 	print '------------------------------------方法二-------------------------------------'
 	second(stra)
+	print '------------------------------------方法三-------------------------------------'
+	third(stra)
+	
