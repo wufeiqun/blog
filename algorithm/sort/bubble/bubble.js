13b8697ec43b23f3e1bb08b98ccad1d04aa7be0b
rocky <wufeiqun@qfpay.com>
2016-06-21 21:20:16 +0800

    finish bubble sort

--- algorithm/sort/bubble/bubble.js
+++ algorithm/sort/bubble/bubble.js
@@ -3,7 +3,7 @@ function bubble_default(array) {
     var length = array.length;
     var tmp;
     if (length <= 1) {
-        return array
+        return array;
     }
     for (let i=1;i<length;i++) {
         for (let j=0;j<length-i;j++) {
@@ -14,8 +14,33 @@ function bubble_default(array) {
             }
         }
     }
-    return array
+    return array;
 }
+
+function bubble_flag(array) {
+    var length = array.length;
+    var tmp;
+    var exchange;
+    if (length <= 1) {
+        return array;
+    }
+    for (let i=1;i<length;i++){
+        exchange = false;
+        for (let j=0;j<length-i;j++){
+            if (array[j] > array[j+1]){
+                tmp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = tmp;
+            }
+        }
+        if (!exchange){
+            break 
+        }
+    }
+    return array;
+}
+
 var array = [5,4,3,2,1]
 
 console.log(bubble_default(array))
+console.log(bubble_flag(array))
